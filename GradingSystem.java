28b6590df4036acda58aef203cd14614f5d0fc0b
Khandokar Ifteakar Ahmed <148181317+ifteakhar00kg@users.noreply.github.com>
2025-08-12 23:07:27 +0600

    Update GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.Scanner;
 
-public class Main {
+public class GradingSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
