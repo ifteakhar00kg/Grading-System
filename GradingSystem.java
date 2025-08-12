91c9554f806c7fddddbf6fb911f5539eec72ef5c
Khandokar Ifteakar Ahmed <148181317+ifteakhar00kg@users.noreply.github.com>
2025-08-12 23:05:43 +0600

    Add files via upload

---
+++ GradingSystem.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("Enter your marks : ");
+        int marks = sc.nextInt();
+
+        //Variables
+        char grade;
+        String message = "";
+
+        //Print grade
+        if (marks >= 90 && marks <= 100) {
+            grade = 'A';
+        } else if (marks >= 80 && marks <= 89) {
+            grade = 'B';
+        } else if (marks >= 70 && marks <= 79) {
+            grade = 'C';
+        } else if (marks >= 60 && marks <= 69) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        //Print message
+        if (grade =='A' || grade == 'B') {
+            message = "Well done";
+        } else if (grade == 'C' || grade == 'D') {
+            message = "Needs improvement";
+        } else {
+            message = "Failed";
+        }
+
+        //Output
+        System.out.println(grade + " : " + message);
+
+        sc.close();
+    }
+}
